068c3613daba188484be25fd09498a3e9649bb40
Alex Dellow <alex_dellow@hotmail.co.uk>
2015-01-14 20:39:30 -0800

    Got Database working

--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -4,23 +4,30 @@ class Gallery extends Application {
 
 	/**
 	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/Gallery
-	 *	- or -  
-	 * 		http://example.com/index.php/Gallery/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/Gallery/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index()
 	{
 		//$this->load->view('gallery');
-		$this->data['pagebody'] = 'about';
+		//$this->data['pagebody'] = 'about';
+		//$this->render();
+		$pix = $this->images->all();
+		
+		foreach($pix as $picture){
+			$cells[] = $this->parser->parse('_cell', (array) $picture, true);
+		}
+		
+		$this->load->library('table');
+		$params = array(
+			'table_open' => '<table class="gallery">',
+			'cell_start' => '<td class="oneimage">',
+			'cell_alt_start' => '<td class="oneimage">'
+		);
+		$this->table->set_template($params);
+		
+		$rows = $this->table->make_columns($cells, 3);
+		$this->data['thetable'] = $this->table->generate($rows);
+		
+		$this->data['pagebody'] = 'gallery';
 		$this->render();
 	}
 }
